cbf1b9905fed3e63b4450507c0d479d114b4afe0
Ehsan-Pazhman <ehsanpazhman@gmail.com>
2026-01-27 23:00:05 +0430

    create patients table migration

---
+++ database/migrations/2026_01_27_150135_create_patients_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('patient_code')->unique();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('father_name')->nullable();
+
+            $table->enum('gender', ['male', 'female']);
+
+            $table->date('date_of_birth')->nullable();
+
+            $table->string('phone')->nullable();
+            $table->string('secondary_phone')->nullable();
+
+            $table->string('national_id')->nullable();
+
+            $table->text('address')->nullable();
+
+            $table->enum('status', ['active', 'inactive', 'deceased'])
+                ->default('active');
+
+            $table->foreignId('created_by')->nullable();
+            $table->foreignId('updated_by')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Indexes
+            $table->index(['last_name', 'phone']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
