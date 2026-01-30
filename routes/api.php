aafbe4e4bbbe757466bfcace6c4e03f3a6efd383
Ehsan-Pazhman <ehsanpazhman@gmail.com>
2026-01-30 20:46:25 +0430

    feat(auth): add users, roles, permissions migrations

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
-use App\Domains\Patient\Controllers\PatientController;
+use App\Livewire\Patient\PatientForm;
+use App\Livewire\Patient\PatientList;
 use Illuminate\Support\Facades\Route;
+use App\Domains\Patient\Controllers\PatientController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('patients', PatientController::class);
     Route::post('patients/{id}/restore', [PatientController::class, 'restore']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/patients', PatientList::class)->name('patients.index');
+    Route::get('/patients/create', PatientForm::class)->name('patients.create');
+    Route::get('/patients/{patientId}/edit', PatientForm::class)->name('patients.edit');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Livewire\Patient\PatientList;
-use App\Livewire\Patient\PatientForm;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/patients', PatientList::class)->name('patients.index');
-    Route::get('/patients/create', PatientForm::class)->name('patients.create');
-    Route::get('/patients/{patientId}/edit', PatientForm::class)->name('patients.edit');
-});
+
